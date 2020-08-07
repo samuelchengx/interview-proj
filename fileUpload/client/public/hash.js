0228d3e6f2af8b6fc8f87d10d0289e5c5bd0385a
samuelchengx <676822037@qq.com>
2020-08-07 17:00:24 +0800

    feat: 文件分片上传和合并

--- fileUpload/client/public/hash.js
+++ fileUpload/client/public/hash.js
@@ -1,26 +1,28 @@
 self.importScripts('http://cdn.bootcss.com/spark-md5/3.0.0/spark-md5.js')
 self.onmessage = async (event) => {
-    let { partList } = event.data;
-    const spark = new self.SparkMd5().ArrayBuffer;
+    let {partList} = event.data;
+    const spark = new self.SparkMD5.ArrayBuffer;
     let percent = 0; //总体计算hash的百分比
-    let perSize = 100/partList.length; // 每计算完part,相当于完成百分之几
-    let buffers = Promise.all(partList.map(({thunk, size})=>{
-        const reader = new FileReader();
-        reader.readAsArrayBuffer(thunk);
-        reader.onload = function (event) {
-            percent += perSize;
-            self.postMessage({
-                percent: Number(percent.toFixed(2))
-            });
-            resolve(event.target.result);
+    let perSize = 100 / partList.length; // 每计算完part,相当于完成百分之几
+    let buffers = await Promise.all(partList.map( ({chunk, size}) => new Promise(function (resolve) {
+            const reader = new FileReader();
+            reader.readAsArrayBuffer(chunk);
+            reader.onload = function (event) {
+                percent += perSize;
+                self.postMessage({
+                    percent: Number(percent.toFixed(2))
+                });
+                resolve(event.target.result);
+            }
         }
-    }));
-    buffers.forEach(buffer=>{
-      spark.append(buffer);
+    )));
+    buffers.forEach(buffer => {
+        spark.append(buffer);
     });
+    //  通知主进程，当前的哈希全部完成，把最终的哈希值给主进程发过去
     self.postMessage({
         percent: 100,
-        hash: spark.hash()
+        hash: spark.end()
     });
     self.close();
 }
