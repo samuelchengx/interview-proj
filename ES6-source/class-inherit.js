808603f971e662d2b1406c5773d76e4db5789fc8
samuelchengx <676822037@qq.com>
2020-08-21 17:46:41 +0800

    feat: 动画实现的6种方案

---
+++ ES6-source/class-inherit.js
@@ -0,0 +1,4 @@
+
+import Parent from "./class-pollify";
+
+
--- ES6-source/class-pollify.js
+++ ES6-source/class-pollify.js
@@ -48,8 +48,9 @@ let Parent = function () {
     return Parent;
 }();
 
+export default Parent;
+
 
 let parent = new Parent('samuelcheng', 29);
 console.log('-----parent-----', parent);
-
 parent.speakSomething();
