8629e59901dcabc5ba10eb7d613a3a16ff6a6684
samuelchengx <676822037@qq.com>
2020-09-08 17:48:38 +0800

    feat: shimo.im

--- js-yl/iv1.js
+++ js-yl/iv1.js
@@ -77,7 +77,7 @@ console.log(object);
  *
  * @param tasks
  * @param callback
- * @returns {undefined}
+ * @return {undefined}
  */
 
 function waterfall(tasks, callback) {
@@ -99,8 +99,7 @@ waterfall([
 
 });
 
-
-// 2.找出二叉树中某两个节点的d第一个共同祖先，不得将其他的节点存储在
+// 2.找出二叉树中某两个节点的第一个共同祖先，不得将其他的节点存储在
 // 另外的数据结构中。
 
 // 例如,给定如下二叉树 root = [3,5,1,6,2,0,8,null,null,7,4]
@@ -130,4 +129,5 @@ function TreeNode(val){
     this.val = val;
     this.left = this.right = null;
 }
+
 /**************************  编程题 END   **********************/
---
+++ js-yl/iv2.js
@@ -0,0 +1,16 @@
+// 1.实现一个搜索组件，通过input输入文字，向后端发送请求，后端返回列表
+class Search extends React.Component {
+    render(){
+        return <div>code here</div>
+    }
+}
+
+// 2、实现一个方法,传入rule，str,返回boolean；
+// rule为 +**{N} +代表一个字符， *代表3个字符 *{N}代表4个字符
+// 例如 +**{4} => "abbbcccc"
+function checkStr(rule, str){
+    let res = false;
+    // code here
+    return res;
+}
+
